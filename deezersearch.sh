9c3bd32cf035fbc5a8dd3c7db491eda32cb1a6f5
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-03-07 12:15:06 +0100

    Update deezersearch.sh

--- deezersearch.sh
+++ deezersearch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #guicmd='bemenu -m $(/home/user/.local/bin/swayfocus) -i --hb #4c566a --hf #88c0d0 --nf #eceff4 --nb #2e3440 --fb #2e3440 --fn Cantarell 14 --tb #2E3440 --tf #5E81AC'
-guicmd='fzf --no-info'
+guicmd='fzf --no-info --height=6'
 
-[ -z $1 ] && type=$(echo -e "track\nalbum\nplaylist" | $guicmd --prompt='type: ' --height=4) || type="$1"
+[ -z $1 ] && type=$(echo -e "track\nalbum\nplaylist" | $guicmd ) || type="$1"
 [ -z $type ] && exit
 #query=$(echo | $guicmd)
 
@@ -14,7 +14,7 @@ query=$(sed \
 	-e 's| |%20|g'\
 	<<< "$query")
 
-if [ -z "$query" ]; then exit; fi
+[ -z "$query" ] && exit
 
 response="$(curl -s "https://www.deezer.com/search/$query")"
 
@@ -24,8 +24,8 @@ case $type in
 	playlist)formated=$(echo "$response" | sed 's|{|\n|g'| grep "\"PLAYLIST_ID\":" | cut -d "," -f 1,4 | cut -d "\"" -f 4,8 );;
 esac
 
-choice=$(echo "$formated" | tr "\"" "-" | cut -d "-" -f 2,3 | $guicmd --height=6)
-if [ -z "$choice" ]; then exit; fi
+choice=$(echo "$formated" | tr "\"" "-" | cut -d "-" -f 2,3 | $guicmd )
+[ -z "$choice" ] && exit
 id=$(echo "$formated" | tr "\"" "-" | grep -Fwm1 "$choice" | cut -d "-" -f 1)
 
 deemix "deezer.com/en/$type/$id"
